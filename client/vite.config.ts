41acef5c00d24e54ea91ef429fb84edaed963571
Louis Bellefemine <bellefemine.louis@gmail.com>
2023-09-16 18:22:34 +0200

    config vite to prevent build on eslint error | #2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
